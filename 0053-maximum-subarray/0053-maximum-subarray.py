c4d40d7e4c250061333876d2dcccc46a2a345aee
Ramanathan-R95 <ramarajendran9585@gmail.com>
2025-06-18 21:12:06 +0530

    Time: 77 ms (53.81%), Space: 21.4 MB (46.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,25 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def subSum(low,high):
-            if low < high:
-                mid=(low+high)//2
-                max1=subSum(low,mid)
-                max2=subSum(mid+1,high)
-                max3=nums[mid]
-                s1=0
-                for j in range(mid,low-1,-1):
-                    s1+=nums[j]
-                    max3=max(s1,max3)
-                s1=0
-                m3=nums[mid+1]
-                for j in range(mid+1,high+1):
-                    s1+=nums[j]
-                    m3=max(s1,m3)
-                max3+=m3
-                return max(max1,max2,max3)
-            elif high==low:
-                return nums[low]
-            
-        return subSum(0,len(nums)-1)
-
+        maxSum=nums[0]
+        sum=0
+        for i in range(len(nums)):
+            sum+=nums[i]
+            maxSum=max(sum,maxSum)
+            if sum<0:
+                sum=0
+        return maxSum
+        
