1ef074e38922f94650854fec8eb480c4e1d20a7d
Ramanathan-R95 <ramarajendran9585@gmail.com>
2025-06-18 21:00:08 +0530

    Time: 12 ms (2.85%), Space: 57.2 MB (43.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,48 +1,35 @@
 class Solution {
-    
+    public int subSum(int []nums,int l,int h){
+        if (l<h){
+            int mid=(l+h)/2;
+            int leftSum=subSum(nums,l,mid);
+            int rightSum=subSum(nums,mid+1,h);
+            int midCrossSum=Integer.MIN_VALUE;
+            int temp=0;
+            int t2=Integer.MIN_VALUE;
+
+            for(int j=mid;j>=l;j--){
+                temp=temp+nums[j];
+                midCrossSum=Math.max(midCrossSum,temp);
+
+            }
+            temp=0;
+            for(int j=mid+1;j<=h;j++){
+                temp+=nums[j];
+                t2=Math.max(t2,temp);
+
+            }
+            midCrossSum=midCrossSum+t2;
+            return (leftSum > rightSum && leftSum >midCrossSum)? leftSum:(rightSum > midCrossSum)? rightSum: midCrossSum;
+        }
+        else if(h==l)
+           return nums[l];
+           
+        else return 0;
+        }
     
     public int maxSubArray(int[] nums) {
-        int maxSum=Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
-            if(maxSum < sum)
-                maxSum=sum;
-           
-            if(sum<0)
-                sum=0;
-        }
-        return maxSum;
-    }    
-
-
-
-
-
-
-
-
-
-            
-            
-            
-            
-            
-            
+        return subSum(nums,0,nums.length-1);
         
-        
-
-        
-            
-
-
-
-
-            
-        }
-
-
-
-        
-
-    
+    }
+}
