066db3530de98cbe75031f2a069ca9f1368b9619
Ramanathan-R95 <ramarajendran9585@gmail.com>
2025-07-20 23:49:45 +0530

    Time: 3 ms (41.78%), Space: 12.6 MB (57.23%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -7,21 +7,21 @@ class Solution(object):
         """
         p1=0
         p2=0
-        res=[]
+        res=set()
         nums1.sort()
         nums2.sort()
         n1=len(nums1)
         n2=len(nums2)
         while p1 < n1 and p2 < n2 :
             if nums1[p1] == nums2[p2]:
-                res.append(nums1[p1])
+                res.add(nums1[p1])
                 p1+=1
                 p2+=1
             elif nums1[p1] > nums2[p2] :
                 p2+=1
             else:
                 p1+=1
-        return list(set(res))
+        return list(res)
 
 
         
