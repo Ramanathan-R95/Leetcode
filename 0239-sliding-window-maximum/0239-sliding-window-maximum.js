724c818a5b13028c94d5e6c757e346c0f6d7191f
Ramanathan-R95 <ramarajendran9585@gmail.com>
2025-08-10 12:27:47 +0530

    Time: 435 ms (33.67%), Space: 88.6 MB (39.42%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -9,8 +9,8 @@ var maxSlidingWindow = function(nums, k) {
     let deque=[];
     let p=0;
     for(r=0;r<nums.length;r++){
-        while( deque.length && nums[deque.slice(-1)[0]] < nums[r]) {deque.pop();}
-        if( deque.length && r-deque[0]+1 >k) deque.shift();
+        while( deque.length && nums[deque[deque.length -1]] < nums[r]) {deque.pop();}
+        while( deque.length && r-deque[0]+1 >k) deque.shift();
         deque.push(r);
         if(r>=k-1)
         result.push(nums[deque[0]]);
