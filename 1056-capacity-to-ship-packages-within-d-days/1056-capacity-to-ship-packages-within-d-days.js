312b1ecef106994d3992e7bf1afd1de033d75f42
Ramanathan-R95 <ramarajendran9585@gmail.com>
2025-09-21 15:23:39 +0530

    Time: 16 ms (25.88%), Space: 58 MB (91.23%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.js
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.js
@@ -4,27 +4,30 @@
  * @return {number}
  */
 var shipWithinDays = function(weights, days) {
-    var getDays = function(weights , i){
+    var getDays = function(weights , i,d){
         let days = 1;
         let curTotal = 0;
         for(let j of weights){
             if(curTotal + j  > i){
                 days++;
+                if(days > d) return false;
                 curTotal = j ;
             }
             else curTotal += j ;
         }
-        return days;
+        return true;
     };
     let left = weights.reduce((max,cur)=>{
         if(cur > max) return cur;
         return max;
     });
-    let right = left * weights.length;
+    let right = weights.reduce((sum,cur)=>{
+        return sum + cur;
+    });
     let res ;
     while (left <= right){
         let mid = Math.floor((left + right) /2 );
-        if(getDays(weights,mid)<=days){
+        if(getDays(weights,mid,days)){
             res = mid;
             right = mid - 1;
 
