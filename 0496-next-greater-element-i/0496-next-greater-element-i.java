6d75bde16646b96f4437d911f01242a19ae2a2d0
Ramanathan-R95 <ramarajendran9585@gmail.com>
2026-02-28 21:39:31 +0530

    Time: 4 ms (73.46%), Space: 45.6 MB (9.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,34 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> m = new HashMap<>() ;
         Stack<Integer> stack = new Stack<>() ;
-        int l = nums1.length ;
-        int result[] = new int[l] ;
-        for(int i = 0 ; i < l ; i++){
-            m.put(nums1[i],i) ;
-        }
-        for(int i = nums2.length - 1  ; i >= 0 ; i--) {
+        HashMap<Integer,Integer> m = new HashMap<>() ;
+        int[] result = new int[nums1.length] ;
+
+        for (int i = nums2.length - 1 ; i>= 0 ; i--){
             int ele = -1 ;
-            while(stack.size() > 0 && nums2[i] > stack.peek()) {
-                stack.pop() ;
+            while ( !stack.isEmpty() && nums2[i] > stack.peek()){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                ele = stack.peek() ;
             }
 
-                if(!stack.isEmpty()) {
-                    ele = stack.peek() ;
 
-                }
 
-                stack.push(nums2[i]) ;
-            if(m.containsKey(nums2[i])){
 
-                result[m.get(nums2[i])] = ele ;
-                
-                
+            m.put(nums2[i] , ele) ;
+            stack.push(nums2[i]);
+        }
+        for(int i= 0 ; i < nums1.length ; i++) {
+            if(m.containsKey(nums1[i])) {
+                result[i] = m.get(nums1[i]) ;
             }
+            else 
+                result[i] = -1 ;
+
         }
         return result ;
 
 
+
+
+
+
+
         
-}
+    }
 }
