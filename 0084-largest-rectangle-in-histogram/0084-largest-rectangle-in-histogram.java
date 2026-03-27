7fb35c63829746e510b0487ac1a31cc14d029176
Ramanathan-R95 <ramarajendran9585@gmail.com>
2026-03-27 12:39:18 +0530

    Time: 60 ms (76.66%), Space: 76.3 MB (87.3%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,46 +1,29 @@
 class Solution {
-    public void genPSE(int[] nums,int[] pse){
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i=0;i<nums.length;i++){
-            while(! stack.isEmpty() && nums[i] <= nums[stack.peek()]) stack.pop();
-            if(stack.isEmpty()) pse[i]= -1;
-            else pse[i]=stack.peek();
-            stack.push(i);
-        }
-    }
-        
-
-
-    
-    public void genNSE(int[] nums,int[] nse){
-        Stack<Integer> stack = new Stack<>();
-        for(int i=nums.length-1;i>=0;i--){
-            while(! stack.isEmpty() && nums[i] <= nums[stack.peek()] ) stack.pop();
-            if( stack.isEmpty())  nse[i] = nums.length;
-            else nse[i] = stack.peek();
-            stack.push(i);
-        }
-    }
-
     public int largestRectangleArea(int[] heights) {
-        int[] nse = new int[heights.length];
-        int[] pse = new int[heights.length];
-        int result=0;
-        int l,r;
-        genNSE(heights,nse);
-        genPSE(heights,pse);
-        for(int i=0;i< heights.length;i++){
-            l=pse[i];
-            r=nse[i];
-            result = Math.max(result,(r-l-1)*heights[i]);
+        Stack<Integer> stack = new Stack<>();
+        int result = Integer.MIN_VALUE;
+        int n = heights.length ;
+        int pse,nse ,ind;
+        for(int i=0;i<n;i++){
+
+            while(!stack.empty() && heights[i] <= heights[stack.peek()]){
+                ind = stack.pop();
+                nse = i ;
+                pse = stack.isEmpty()? -1 : stack.peek();
+                result = Math.max(result,(nse-pse-1)* heights[ind]);
 
 
 
-
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            ind = stack.pop();
+            pse = stack.isEmpty()? -1 : stack.peek();
+            nse = n ;
+            result = Math.max(result,(nse-pse-1)* heights[ind]);
         }
         return result;
-        
         
     }
 }
