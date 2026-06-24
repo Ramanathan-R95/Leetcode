2997bee46c23a163d22e5579491f781bd0e0755b
Ramanathan-R95 <ramarajendran9585@gmail.com>
2026-06-24 18:54:18 +0530

    Time: 128 ms (26.39%), Space: 50.9 MB (8.38%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,43 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def dfs(self,nodes,node,target):
-        if not node:
-            return False
-        nodes.append(node)
-        if node == target:
-            return True
-        if self.dfs(nodes,node.left,target) or self.dfs(nodes,node.right,target):
-            return True
-        nodes.pop()
-        return False
-        
-
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        stack_p = []
-        stack_q = []
-        self.dfs(stack_p,root,p)
-        self.dfs(stack_q,root,q)
-        lca = None
-        l = min(len(stack_p),len(stack_q))
-     
-        for i in range(l):
-            if stack_p[i] == stack_q[i]:
-                lca = stack_p[i]
-            else:
-                break
-        return lca
-        
-        
-        
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left and right:
+            return root
+        return left if left else right
