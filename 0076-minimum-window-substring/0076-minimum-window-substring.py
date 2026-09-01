966f435f00a12c04655e425558e477652e062b44
Ramanathan-R95 <ramarajendran9585@gmail.com>
2026-09-01 21:42:32 +0530

    Time: 695 ms (5.06%), Space: 19.8 MB (37.45%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,41 +1,30 @@
-class Solution(object):
-    
-    
+class Solution:
+    def check(self,freq):
+        for i in freq:
+            if i < 0:
+                return False
+        return True
+    def minWindow(self, s: str, t: str) -> str:
+        freq = [0] * 256
+        for i in t:
+            freq[ord(i) - ord("A")] -= 1
+        l = 0
+        r = 0
+        temp = [-1, -1]
 
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        result=(-1,-1)
-        resultLen = float("inf")
-        count = 0
-        counts=[0]*58
-        l=0
-        r=0
-        for i in range(len(t)):
-           counts[ord(t[i])-ord("A")]=counts[ord(t[i])-ord("A")]-1
-           
+        if len(t) > len(s):
+            return ""
+        flag = float("inf")
         for r in range(len(s)):
-           
-            if counts[ord(s[r])-ord("A")] < 0:
-                count+=1
-            counts[ord(s[r])-ord("A")]=counts[ord(s[r])-ord("A")]+1
-            
-            while count == len(t):
-                if r-l+1 < resultLen :
-                    resultLen = r-l+1
-                    result = (l,r+1)
-                counts[ord(s[l])-ord("A")]-=1
-                if counts[ord(s[l])-ord("A")] < 0:
-                    count-=1
-                l+=1
-        return s[result[0]:result[1]]
-
-                
+            freq[ord(s[r]) - ord("A")] += 1
+            while self.check(freq):
+                if r - l + 1 < flag:
+                    flag = r - l + 1
+                    temp[0] = l
+                    temp[1] = r
+                freq[ord(s[l]) - ord("A")] -= 1
+                l += 1
+        return s[temp[0] : temp[1] + 1]
 
 
 
-
-        
